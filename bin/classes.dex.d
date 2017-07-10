711054520bc8ec37bd4b5b47ba6cc62e697adf66
zhenzhen15 <janyzhen15@163.com>
2017-07-10 22:46:21 +0800

    o

--- bin/classes.dex.d
+++ bin/classes.dex.d
@@ -1,6 +1,7 @@
 C:\Users\Administrator\Desktop\Sample_Uia\bin\classes.dex : \
 C:\Users\Administrator\Desktop\Sample_Uia\bin\classes\com\hj\autotest\AutoTest.class \
 C:\Users\Administrator\Desktop\Sample_Uia\bin\classes\com\hj\autotest\QuanShi.class \
+C:\Users\Administrator\Desktop\Sample_Uia\bin\classes\com\hj\autotest\QuanShi1.class \
 C:\Users\Administrator\Desktop\Sample_Uia\bin\classes\com\hj\autotest\UiautomatorAsssistant.class \
 C:\Users\Administrator\Desktop\Sample_Uia\bin\classes\com\hj\autotest\UiAutomatorTool.class \
 C:\Users\Administrator\Desktop\Sample_Uia\bin\classes\quanshipacage\QuanShi.class \
